f3b5d9741c864112320d383e27a5762c5f58aafe
abhitrip <atripath@eng.ucsd.edu>
2017-04-12 13:01:26 -0700

    Adding files for plain adding graph

---
+++ miniflow.py
@@ -0,0 +1,112 @@
+class Node(object):
+    def __init__(self,inbound_nodes=[]):
+        # Node(s) from which this Node receives values
+        self.inbound_nodes = inbound_nodes
+        # Node(s) to which this Node passes values
+        self.outbound_nodes = []
+        # For each inbound node , add this node as outbound node
+        for n in self.inbound_nodes:
+            n.outbound_nodes.append(self)
+
+        # A calculated value
+        self.value = None
+
+    # Placeholder for forward propagation
+    def forward(self):
+        """
+        forward propagation
+        Compute the output value based on inbound_nodes and
+        store result in self.value
+        """ 
+        raise NotImplemented
+
+class Input(Node):
+    def __init__(self):
+        # An input Node has no inbound nodes
+        # so no need to pass any to node instantiator
+        Node.__init__(self)
+
+    # NOTE: Input node is the only node where the value
+    # may be passed as an argument to forward().
+    #
+    # All other node implementations should get the value
+    #
+    # of the previous node from self.inbound_nodes
+    # Example:
+    # val0 = self.inbound_nodes[0].value
+    def forward(self,value=None):
+        # Overwrite the value if one is passed in.
+        if value is not None:
+            self.value = value
+
+class Add(Node):
+    def __init__(self,x,y):
+        # You could access `x` and `y` in forward with
+        # self.inbound_nodes[0] (`x`) and self.inbound_nodes[1] (`y`)
+        Node.__init__(self,[x,y])
+    def forward(self):
+        """
+        Set the value of this node (`self.value`) to the sum of its inbound_nodes.
+        """
+        xvalue = self.inbound_nodes[0].value
+        yvalue = self.inbound_nodes[1].value
+        self.value = xvalue+yvalue
+
+def topological_sort(feed_dict):
+    """
+    Sort generic nodes in topological order using Kahn's Algorithm.
+
+    `feed_dict`: A dictionary where the key is a `Input` node and the value is the respective value feed to that node.
+
+    Returns a list of sorted nodes.
+    """
+
+    input_nodes = [n for n in feed_dict.keys()]
+
+    G = {}
+    nodes = [n for n in input_nodes]
+    while len(nodes) > 0:
+        n = nodes.pop(0)
+        if n not in G:
+            G[n] = {'in': set(), 'out': set()}
+        for m in n.outbound_nodes:
+            if m not in G:
+                G[m] = {'in': set(), 'out': set()}
+            G[n]['out'].add(m)
+            G[m]['in'].add(n)
+            nodes.append(m)
+
+    L = []
+    S = set(input_nodes)
+    while len(S) > 0:
+        n = S.pop()
+
+        if isinstance(n, Input):
+            n.value = feed_dict[n]
+
+        L.append(n)
+        for m in n.outbound_nodes:
+            G[n]['out'].remove(m)
+            G[m]['in'].remove(n)
+            # if no other incoming edges add to S
+            if len(G[m]['in']) == 0:
+                S.add(m)
+    return L
+
+def forward_pass(output_node, sorted_nodes):
+    """
+    Performs a forward pass through a list of sorted nodes.
+
+    Arguments:
+
+        `output_node`: A node in the graph, should be the output node (have no outgoing edges).
+        `sorted_nodes`: A topologically sorted list of nodes.
+
+    Returns the output Node's value
+    """
+
+    for n in sorted_nodes:
+        n.forward()
+
+    return output_node.value
+
